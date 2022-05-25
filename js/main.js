933c68e8ace5b35bb982aa05d897147d70c8a962
montlein <montvlein@gmail.com>
2022-05-25 00:05:57 -0400

    fix menu active scroll

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 const navlinks = document.querySelectorAll(".navbar li a")
 const sections = document.querySelectorAll("section")
-const inicio = document.querySelector("#inicio")
-const somos = document.querySelector("#somos")
-const servicios = document.querySelector("#servicios")
-const niveles = document.querySelector("#niveles")
-const contacto = document.querySelector("#contacto")
 const observador = new IntersectionObserver(active, {
     root: null,  // hace referencia al viewport
     rootMargin: "0px",
@@ -17,37 +12,29 @@ function obrservar(arr) {
     })
 }
 
-obrservar([inicio, somos, servicios, niveles, contacto])
+obrservar(sections)
 
-window.addEventListener("load",function(e){
-    for (const element of navlinks) {
-        element.addEventListener('click', e=>{
-            unSelectNavItem()
-            element.classList.add("active")
-        })
-    }
+// window.addEventListener("load",function(e){
+//     for (const element of navlinks) {
+//         element.addEventListener('click', e=>{
+//             unSelectNavItem()
+//             element.classList.add("active")
+//         })
+//     }
 
-    function unSelectNavItem(){
-        for (const element of navlinks) {
-            element.classList.remove("active")
-        }
-    }
+//     function unSelectNavItem(){
+//         for (const element of navlinks) {
+//             element.classList.remove("active")
+//         }
+//     }
 
-})
+// })
 
 function active(elementos, observador) {
-    // for (let index = 0; index < navlinks.length; index++) {
-    //     const anchor = navlinks[index]
-    //     const section = elementos[index]
-    //     console.log(section.target, anchor);
-    // }
-    elementos.forEach((section, index) => {
+    elementos.forEach((section) => {
+        index = Array.from(sections).indexOf(section.target)
         if (section.isIntersecting) {
-            // console.log(index, section, navlinks[index]);
             navlinks[index].classList.add("active")
-            console.log(navlinks[index].attributes.href.value)
-            console.log("--- espacio ---")
-            console.log(section.target.id)
         } else {
             navlinks[index].classList.remove("active")
         }
